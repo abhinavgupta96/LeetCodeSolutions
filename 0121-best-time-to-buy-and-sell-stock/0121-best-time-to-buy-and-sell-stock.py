adff87a614e7daceb1b86ccbcd0a1bf0645d50d0
Abhinav Gupta <abhinavgupta0907@gmail.com>
2022-11-04 16:21:27 -0700

    Time: 1011 ms (96.46%), Space: 25 MB (38.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -6,8 +6,19 @@ class Solution:
         max_prof = 0
         min_price = prices[0]
         for i in range(len(prices)):
-            if prices[i] < min_price:
+            if prices[i] < min_price: ##compare every value with min value, i.e time to buy
                 min_price = prices[i]
-            elif prices[i] - min_price > max_prof:
+            elif prices[i] - min_price > max_prof: ##if not min_value, compute profit, if greater than current max profit
                 max_prof = prices[i] - min_price
         return max_prof
+    ##approach2 - from neetcode
+    # l,r = 0,1 ##left is where we buy, right is where we sell
+    # max_prof = 0
+    # while r < len(prices):
+    #     if prices[l] < prices[r]: #if price to buy is lower than sell
+    #         prof = prices[r] - prices[l]
+    #         max_prof = max(prof,max_prof) #checks if current profit is bigger than current max profit
+    #     else:
+    #         l = r #if buy price is higher than sell, then move current buy price to sell price
+    #     r+=1 ##increment r in either case
+    # return max_prof
