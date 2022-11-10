0b5b00fc9ca0740103fcd3280d3192b79a84bb03
Abhinav Gupta <abhinavgupta0907@gmail.com>
2022-11-09 18:51:43 -0700

    Update 0242-valid-anagram.py

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(t) == len(s):
-            hash_s = {}
+            # return sorted(s)==sorted(t) sorts the two strings and compare them
+            hash_s = {} #build a hashmap of each string and then compare them
             hash_t = {}
             for i in list(s):
                 if i not in hash_s:
