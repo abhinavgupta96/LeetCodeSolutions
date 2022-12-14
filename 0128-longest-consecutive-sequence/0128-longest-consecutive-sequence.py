a8143b49d50a8a40e9ee5d86a872c3297197c20c
Abhinav Gupta <abhinavgupta0907@gmail.com>
2022-12-14 12:27:15 -0700

    Update 0128-longest-consecutive-sequence.py

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,20 +1,20 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         longest = 0 
-        s = set(nums)
+        s = set(nums) ##create a set, helps dealing with duplicate values
         for num in nums:
             cur_longest = 1
             j = 1
-            while num - j in s: 
-                s.remove(num - j)
+            while num - j in s: ##check from current value, if values smaller than it exits
+                s.remove(num - j) ##if value smaller exits, remove from set
                 cur_longest = cur_longest + 1
                 j =  j + 1
             j = 1
-            while num + j in s: 
+            while num + j in s: ##check if value greater than current exits, if they do remove it from the set
                 s.remove(num + j)
                 cur_longest = cur_longest + 1
                 j = j + 1
-            longest = max(longest, cur_longest)
+            longest = max(longest, cur_longest) ##assign longest value as the max b/w longest sequence and current longest
         return longest
         
                 
