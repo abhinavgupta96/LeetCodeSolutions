0b48abacb495f6a74e6f1318b559c968a3caafa2
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-08-16 10:33:37 -0700

    Time: 461 ms (97.84%) | Memory: 31 MB (61.49%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,12 +1,21 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hash = {}
-        for i in nums:
-            if i not in hash:
-                hash[i] = 1
-            else:
-                hash[i]+=1
-        for values in hash.values():
-            if values > 1:
-                return True
-        
+            output=False
+        # hash = {} create a dictionary and pass through all elements, with the value being the number of times that number appears in array
+        # for i in range(len(nums)):
+        #     if nums[i] not in hash:
+        #         hash[nums[i]] = 1
+        #     else:
+        #         hash[nums[i]] +=1
+        # for j in hash.values():
+        #     if j>1:
+        #         output = True
+        #         return output
+            hashset = set() ##this approach is where we create a set, set cannot contain duplicate values in python
+            for n in nums:
+                if n in hashset:
+                    output = True
+                    return output
+                else:
+                    hashset.add(n)
+            return output
