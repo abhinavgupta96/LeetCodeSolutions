b6dbd08941f1b5d10ad146cbe24c714dcf756373
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-08-21 21:14:21 -0700

    Time: 93 ms (92.73%) | Memory: 19.4 MB (94.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,20 @@
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-         ##default dict gives a dict like object with a default value, here we use default values as empty list
+        ##default dict gives a dict like object with a default value, here we use default values as empty list
         # res = defaultdict(list) ##initializing default dict
         # for i in strs:
-        #     temp = [0]*26 ##create an array with 26 elements, these represents characters a to z. So any string with same characters will end up with same temp list
+        ##create an array with 26 elements, these represents characters a to z. So any string with same characters will end up with same temp list
+        #     temp = [0]*26 
         #     for c in i:
-        #         val = ord(c) - ord("a") ##to figure out index of character in temp, we use ascii values and we subtract by a coz we need asbolute values
+        #         val = ord(c) - ord("a") 
+        ##to figure out index of character in temp, we use ascii values and we subtract by a coz we need asbolute values
         #         temp[val]+=1
-        #     res[tuple(temp)].append(i) ##dictonary in python donot allow list to be a key, so we convert our list into a tuple and append the value of string into the values section
+        #     res[tuple(temp)].append(i) 
+        ##dictonary in python donot allow list to be a key, so we convert our list into a tuple and append the value of string into the values section
         # return res.values()
+        
+        #This approach uses a sorted value of a string as a key in a hashmap, if the key exists we add the string to the value of that key, in the end we return the list of values.
         result = defaultdict(list)
         ans = []
         for i in range(len(strs)):
