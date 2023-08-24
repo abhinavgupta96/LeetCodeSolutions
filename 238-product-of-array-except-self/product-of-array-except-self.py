abb4ee453bf158ea152af32ee4329e02b3c4a393
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-08-23 21:29:36 -0700

    Time: 197 ms (75.73%) | Memory: 24.3 MB (39.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,38 @@
+import math
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #my approach - gives memory exceed error
+        # prefix = [0 for i in range(len(nums))]
+        # postfix = [0 for i in range(len(nums))]
+        # ans = [0 for i in range(len(nums))]
+        # for i in range(len(nums)):
+        #     if i!=0: 
+        #         tmp_list = nums.copy()
+        #         tmp_list = tmp_list[:i]
+        #         val = math.prod(tmp_list)
+        #         prefix[i] = val
+        #     else:
+        #         prefix[i] = 1
+        # for i in range(len(nums)):
+        #     if i!=len(nums)-1 : 
+        #         tmp_list = nums.copy()
+        #         tmp_list = tmp_list[i+1:]
+        #         val = math.prod(tmp_list)
+        #         postfix[i] = val
+        #     else:
+        #         postfix[i] = 1
+        # for i in range(len(nums)):
+        #     ans[i] = postfix[i]*prefix[i]
+        # return ans
         
-        result = len(nums)*[1]   # create an array to store the result and initialize it with 1
-        pre = len(nums)*[1]      # create an array to store prefix products and initialize it with 1
-        post= len(nums)*[1]      # create an array to store suffix products and initialize it with 1
-        pref, postf = 1 , 1      # initialize prefix and suffix products to 1
-        
-        # calculate prefix products
-        for i in range(len(nums)):
-            pre[i]= pref       # store the prefix product of nums[0:i] in pre[i]
-            pref *= nums[i]    # update the prefix product by multiplying with the current number
-            
-        # calculate suffix products
-        for i in range(len(nums)-1,-1,-1):
-            post[i]= postf      # store the suffix product of nums[i+1:n] in post[i]
-            postf *=nums[i]     # update the suffix product by multiplying with the current number
-        
-        result[0]= post[0]       # set result[0] to the suffix product of the last element
-        result[len(nums)-1] = pre[len(nums)-1]   # set result[n-1] to the prefix product of the first element
-        
-        # calculate product of prefix and suffix values
-        for i in range (1,len(nums)-1):
-            result[i]=pre[i]*post[i]   # compute the product of prefix and suffix values
-        
-        return result     # return the final result array
+        # Neetcode approach - have to understand it 
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
