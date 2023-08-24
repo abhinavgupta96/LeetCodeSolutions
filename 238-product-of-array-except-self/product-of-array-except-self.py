73c849e1ffcc399c6adce4205a5da40f03025deb
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-08-23 22:19:49 -0700

    Time: 187 ms (91.88%) | Memory: 23.9 MB (77.87%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -6,16 +6,44 @@ class Solution:
         answer = [1] * n
         
         # Calculate the products of all elements to the left of each index
+        # Since our final answer will be the result of prefix * postfix
+        # In the first pass we calculate the prefix value for each element in the input
+        # We also iteratively update the left product value after each calculation
         left_product = 1
         for i in range(n):
             answer[i] *= left_product
             left_product *= nums[i]
         
         # Calculate the products of all elements to the right of each index
+        # The first pass will give us a the answer array with the prefix value for each of the element in the input array.
+        # In this pass we calculate the postfix values for them, iteratively updating the right product values as well.
         right_product = 1
         for i in range(n - 1, -1, -1):
             answer[i] *= right_product
             right_product *= nums[i]
         
         return answer
+        #my approach - gives memory exceed error
+        # prefix = [0 for i in range(len(nums))]
+        # postfix = [0 for i in range(len(nums))]
+        # ans = [0 for i in range(len(nums))]
+        # for i in range(len(nums)):
+        #     if i!=0: 
+        #         tmp_list = nums.copy()
+        #         tmp_list = tmp_list[:i]
+        #         val = math.prod(tmp_list)
+        #         prefix[i] = val
+        #     else:
+        #         prefix[i] = 1
+        # for i in range(len(nums)):
+        #     if i!=len(nums)-1 : 
+        #         tmp_list = nums.copy()
+        #         tmp_list = tmp_list[i+1:]
+        #         val = math.prod(tmp_list)
+        #         postfix[i] = val
+        #     else:
+        #         postfix[i] = 1
+        # for i in range(len(nums)):
+        #     ans[i] = postfix[i]*prefix[i]
+        # return ans
 
