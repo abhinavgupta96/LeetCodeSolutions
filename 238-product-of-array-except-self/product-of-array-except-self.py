526730f7aaa9c4613f99659dd54e5c7b4e07e4ed
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-08-23 21:33:19 -0700

    Time: 172 ms (99.77%) | Memory: 24 MB (69.48%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,38 +1,21 @@
 import math
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        #my approach - gives memory exceed error
-        # prefix = [0 for i in range(len(nums))]
-        # postfix = [0 for i in range(len(nums))]
-        # ans = [0 for i in range(len(nums))]
-        # for i in range(len(nums)):
-        #     if i!=0: 
-        #         tmp_list = nums.copy()
-        #         tmp_list = tmp_list[:i]
-        #         val = math.prod(tmp_list)
-        #         prefix[i] = val
-        #     else:
-        #         prefix[i] = 1
-        # for i in range(len(nums)):
-        #     if i!=len(nums)-1 : 
-        #         tmp_list = nums.copy()
-        #         tmp_list = tmp_list[i+1:]
-        #         val = math.prod(tmp_list)
-        #         postfix[i] = val
-        #     else:
-        #         postfix[i] = 1
-        # for i in range(len(nums)):
-        #     ans[i] = postfix[i]*prefix[i]
-        # return ans
+        n = len(nums)
+        # Initialize the answer array
+        answer = [1] * n
         
-        # Neetcode approach - have to understand it 
-        res = [1] * (len(nums))
-
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        # Calculate the products of all elements to the left of each index
+        left_product = 1
+        for i in range(n):
+            answer[i] *= left_product
+            left_product *= nums[i]
+        
+        # Calculate the products of all elements to the right of each index
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= right_product
+            right_product *= nums[i]
+        
+        return answer
 
