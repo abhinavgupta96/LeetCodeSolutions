471dd9d6992cd35a06918b15919098a4d8cf65ad
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-09-04 11:27:20 -0700

    Update top-k-frequent-elements.py

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,16 @@
-from collections import defaultdict
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # my approach create a dict containing the number and their count
         # then use a sorted function to sort the dictionary based on values instead of keys
         # to do that we use a lambda function inside sorted(), where the key of the sorted func. is being passed  as the value of the dictionary
-        # sorted function internally based on the value of the dictionary will keep an order of the keys of that dictionary
-        ans = defaultdict(list)
+        # In the lambda function lambda x: ans[x], we are using the lambda function to sort the keys (x) of the ans dictionary based on their 
+        # associated values (ans[x]). So, it doesn't return the keys explicitly but rather sorts them based on the values. 
+        hash = {}
         for i in range(len(nums)):
-            ans[nums[i]].append(nums[i])
-        for i,j in ans.items():
-            value = len(j)
-            ans[i] = value
-        sorted_value = sorted(ans, key= lambda x : ans[x], reverse= True)
-        sorted_value = sorted_value[:k]
-        return sorted_value
+            hash[nums[i]] = hash.get(nums[i], 0) + 1
+        sorted_value = sorted(hash, key= lambda x : hash[x], reverse=True)
+        return sorted_value[:k]
     
         # Neetcode approach is to create a hashmap of the dictionary with key as the number and the value as the count of its appreance in input
         # It then proceeds to create an array of arrays of size 1 more than the lenght of input
