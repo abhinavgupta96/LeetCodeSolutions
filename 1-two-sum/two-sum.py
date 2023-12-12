6f3d44b34f40aacdc568ab013d15b82928e9c073
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-12-12 10:49:32 +0530

    Time: 583 ms (37.23%) | Memory: 17.1 MB (84.66%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,11 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # in this approach instead of storing the indexes of the occurence of a number, we just store the latest occurence which could be max of 2 only ans the problem is "two sum".
-        hashmap = {}
+        hashmap = defaultdict(list)
         for i in range(len(nums)):
-            hashmap[nums[i]] = i
-        for i in range(len(nums)):
-            output = []
             diff = target - nums[i]
-            if diff in hashmap and hashmap[diff]!=i:
-                output.append(i)
-                output.append(hashmap[diff])
-                return output
-        return []
-            
+            if diff in nums and i != nums.index(diff):
+                hashmap[nums[i]].append(i)
+                hashmap[nums[i]].append(nums.index(diff))
+        return list(hashmap.values())[0]
+        
