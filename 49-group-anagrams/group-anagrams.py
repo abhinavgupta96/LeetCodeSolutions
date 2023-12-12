6337dd62abb70070a4fc33a7eacfb7fbadc38d11
Abhinav Gupta <abhinavgupta0907@gmail.com>
2023-12-12 12:01:07 +0530

    Time: 106 ms (35.87%) | Memory: 19.7 MB (70.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,27 +1,13 @@
-from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ##default dict gives a dict like object with a default value, here we use default values as empty list
-        # res = defaultdict(list) ##initializing default dict
-        # for i in strs:
-        ##create an array with 26 elements, these represents characters a to z. So any string with same characters will end up with same temp list
-        #     temp = [0]*26 
-        #     for c in i:
-        #         val = ord(c) - ord("a") 
-        ##to figure out index of character in temp, we use ascii values and we subtract by a coz we need asbolute values
-        #         temp[val]+=1
-        #     res[tuple(temp)].append(i) 
-        ##dictonary in python donot allow list to be a key, so we convert our list into a tuple and append the value of string into the values section
-        # return res.values()
-        
         #This approach uses a sorted value of a string as a key in a hashmap, if the key exists we add the string to the value of that key, in the end we return the list of values.
-        result = defaultdict(list)
         ans = []
+        hashmap = defaultdict(list)
         for i in range(len(strs)):
             val1 = sorted(strs[i])
             value = ""
             value = value.join(val1)
-            result[value].append(strs[i])
-        for i in result.values():
+            hashmap[value].append(strs[i])
+        for i in hashmap.values():
             ans.append(i)
         return ans
