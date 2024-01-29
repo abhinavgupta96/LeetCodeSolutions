564f41ee4afaceff62ff2a31c0eec9aa2bad2b3f
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-01-29 12:17:31 -0700

    Time: 170 ms (66.22%) | Memory: 23.9 MB (85.97%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,49 +1,16 @@
-import math
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        # Initialize the answer array
-        answer = [1] * n
-        
-        # Calculate the products of all elements to the left of each index
-        # Since our final answer will be the result of prefix * postfix
-        # In the first pass we calculate the prefix value for each element in the input
-        # We also iteratively update the left product value after each calculation
+        '''
+        The quickest approach satisfying the problem statement involves 2 passes over the nums list. For the first pass from the left, we use a local variable called as "left_product". In our ans array which we multiply the element over the current index value with that of the left_product, and then we update the value of left_product by multiplying it with the value from nums list at the same index. We do the same for the second pass where we approach the nums array from the back and keep updating the ans array.
+        '''
         left_product = 1
-        for i in range(n):
-            answer[i] *= left_product
-            left_product *= nums[i]
-        
-        # Calculate the products of all elements to the right of each index
-        # The first pass will give us a the answer array with the prefix value for each of the element in the input array.
-        # In this pass we calculate the postfix values for them, iteratively updating the right product values as well.
         right_product = 1
-        for i in range(n - 1, -1, -1):
-            answer[i] *= right_product
-            right_product *= nums[i]
-        
-        return answer
-        #my approach - gives memory exceed error
-        # prefix = [0 for i in range(len(nums))]
-        # postfix = [0 for i in range(len(nums))]
-        # ans = [0 for i in range(len(nums))]
-        # for i in range(len(nums)):
-        #     if i!=0: 
-        #         tmp_list = nums.copy()
-        #         tmp_list = tmp_list[:i]
-        #         val = math.prod(tmp_list)
-        #         prefix[i] = val
-        #     else:
-        #         prefix[i] = 1
-        # for i in range(len(nums)):
-        #     if i!=len(nums)-1 : 
-        #         tmp_list = nums.copy()
-        #         tmp_list = tmp_list[i+1:]
-        #         val = math.prod(tmp_list)
-        #         postfix[i] = val
-        #     else:
-        #         postfix[i] = 1
-        # for i in range(len(nums)):
-        #     ans[i] = postfix[i]*prefix[i]
-        # return ans
+        ans = [1]*len(nums)
+        for i in range(len(nums)):
+            ans[i]*= left_product
+            left_product *=nums[i]
 
+        for i in range(len(nums) - 1, -1 ,-1):
+            ans[i]*=right_product
+            right_product *=nums[i]
+        return ans
