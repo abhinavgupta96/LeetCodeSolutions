b86caa095469ca50b8f0dcf184749367835c9294
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-02-07 15:07:22 -0700

    Time: 321 ms (99.22%) | Memory: 31.8 MB (69.36%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -6,28 +6,15 @@ class Solution:
         Logic is to find the start of each of the range that might exist in the given array, if we find that a number 1 greater than current 
         number is present in the set we continue to iterate, otherwise we move to the next number in the set.
         '''
-        # set_nums = set(nums)
-        # longest = 0
-        # for i in nums:
-        #     length = 0
-        #     while i + length in nums : 
-        #         length+=1
-        #         longest = max(longest,length)
-        # return longest
-        longest = 0 
-        s = set(nums) ##create a set, helps dealing with duplicate values
-        for num in nums:
-            cur_longest = 1
-            j = 1
-            while num - j in s: ##check from current value, if values smaller than it exits
-                s.remove(num - j) ##if value smaller exits, remove from set
-                cur_longest = cur_longest + 1
-                j =  j + 1
-            j = 1
-            while num + j in s: ##check if value greater than current exits, if they do remove it from the set
-                s.remove(num + j)
-                cur_longest = cur_longest + 1
-                j = j + 1
-            longest = max(longest, cur_longest)
+        set_nums = set(nums)
+        longest = 0
+        
+        for i in set_nums : 
+            if i-1 not in set_nums : 
+                y = i+1
+                while y in set_nums:
+                    y+=1
+                sequence = y - i
+                longest = max(longest,sequence)
         return longest
             
