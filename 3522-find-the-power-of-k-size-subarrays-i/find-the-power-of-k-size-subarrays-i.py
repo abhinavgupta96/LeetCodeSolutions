0e4915c3a0a78dac999ca3064def89cad756bfb3
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-12-16 14:01:14 -0700

    Time: 7 ms (63.32%) | Memory: 17.6 MB (5.35%) - LeetSync

---
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        consecutiveCount = 1
+        l = 0
+
+        for r in range(len(nums)):
+            if r>0 and nums[r-1] +1 == nums[r]:
+                consecutiveCount+=1 
+            if r - l +1 > k:
+                if nums[l]+1 == nums[l+1]:
+                    consecutiveCount-=1
+                l+=1 
+            if r -l + 1 ==k:
+                res.append(nums[r] if consecutiveCount==k else -1)
+        return res
