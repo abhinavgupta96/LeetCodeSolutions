6107c1439b4554f1ffb686d195715052f4cf4da9
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-12-16 20:58:48 -0700

    Time: 11 ms (80.71%) | Memory: 20.7 MB (39.46%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #This approach uses a sorted value of a string as a key in a hashmap, if the key exists we add the string to the value of that key, in the end we return the list of values.
-        ans = []
-        hashmap = defaultdict(list)
-        for i in range(len(strs)):
-            val1 = sorted(strs[i])
-            value = ""
-            value = value.join(val1)
-            hashmap[value].append(strs[i])
-        for i in hashmap.values():
-            ans.append(i)
-        return ans
+        hashdict = defaultdict(list)
+        for i in strs:
+            hashkey = tuple(sorted(i))
+            hashdict[hashkey].append(i)
+        return list(hashdict.values())
+        
