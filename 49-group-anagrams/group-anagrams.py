fec8835f4618a178a9863a49b640a7cfdb594d3a
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-12-16 21:02:12 -0700

    Time: 19 ms (37.49%) | Memory: 22.3 MB (6.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # hashdict = defaultdict(list)
+        # for i in strs:
+        #     hashkey = tuple(sorted(i))
+        #     hashdict[hashkey].append(i)
+        # return list(hashdict.values())
         hashdict = defaultdict(list)
-        for i in strs:
-            hashkey = tuple(sorted(i))
-            hashdict[hashkey].append(i)
+        
+        for word in strs:
+            t = [0]*26
+            for c in word:
+                t[ord(c) - ord('a')]+=1
+            key = tuple(t)
+            hashdict[key].append(word)
         return list(hashdict.values())
+
+                
         
