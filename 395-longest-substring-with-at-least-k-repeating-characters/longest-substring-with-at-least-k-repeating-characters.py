db8c70a54f8bd920e34108f2b4c9432179076db3
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-12-19 19:59:18 -0700

    Time: 771 ms (26.49%) | Memory: 57.2 MB (8.23%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.py
@@ -1,21 +1,12 @@
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
-        def dfs(s, k) : 
-            if len(s)==0 :
-                return 0
-
-            freq = {}
-            for char in s:
-                if char in freq:
-                    freq[char]+=1
-                else:
-                    freq[char]=1
-            
-            for i ,char in enumerate(s):
-                if freq[char] < k:
-                    left = dfs(s[:i], k)
-                    right = dfs(s[i+1:], k)
+        def dfs(s,k):
+            freq_count = collections.Counter(s)
+            for i, c in enumerate(s) :
+                  if freq_count[c] < k:
+                    left = dfs(s[:i],k)
+                    right = dfs(s[i+1:],k)
                     return max(left, right)
-            return (len(s))
-        maxLen = dfs(s, k)
-        return maxLen
+            return len(s)
+        return dfs(s,k)
+        
