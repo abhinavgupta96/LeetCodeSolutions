91fbf4685216876735103fc623392a2b0feeb75d
Abhinav Gupta <abhinavgupta0907@gmail.com>
2024-12-21 10:19:54 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (13.63%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,12 +1,21 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        hashDict = defaultdict(int)
-        for n in range(len(arr)):
-            hashDict[arr[n]] = n
+        # hashDict = defaultdict(int)
+        # for n in range(len(arr)):
+        #     hashDict[arr[n]] = n
         
-        for n in range(len(arr)):
-            d = arr[n]*2
-            if d in hashDict and hashDict[d]!=n:
+        # for n in range(len(arr)):
+        #     d = arr[n]*2
+        #     if d in hashDict and hashDict[d]!=n:
+        #         return True
+        # return False
+        # Uses Set instead of Hashmap
+        seen = set()
+        
+        for val in arr:
+            if val * 2 in seen or (val % 2 == 0 and val // 2 in seen):
                 return True
+            seen.add(val)
+        
         return False
         
