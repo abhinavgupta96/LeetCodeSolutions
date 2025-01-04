1f1bb631a9585eb84097160e3c763a1e00dbc3b7
Abhinav Gupta <abhinavgupta0907@gmail.com>
2025-01-04 12:00:26 -0700

    Time: 10 ms (77.37%) | Memory: 17.7 MB (33.06%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,11 +2,11 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        sDict = {}
-        tDict = {}
-        for i in s:
-            sDict[i] = sDict.get(i,0) + 1
-        for j in t :
-            tDict[j] = tDict.get(j, 0) + 1
-        return sDict==tDict
         
+        s_list = [0]*26
+        t_list = [0]*26
+
+        for i in range(len(s)):
+            s_list[ord(s[i])- ord('a')]+=1
+            t_list[ord(t[i]) - ord('a')]+=1
+        return s_list == t_list
