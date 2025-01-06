773edd498fdf04b3409d06f718fdf5185fd0b96a
Abhinav Gupta <abhinavgupta0907@gmail.com>
2025-01-06 11:20:56 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (20.03%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,20 +1,17 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-
         for i in range(len(s)):
             if s[i]!="]":
                 stack.append(s[i])
             else:
                 substr = ""
                 while stack[-1]!="[":
-                    substr = stack.pop() + substr
-                # below pop removes the opening bracket
-                stack.pop() 
+                    substr= stack.pop() + substr
+                stack.pop()
                 k = ""
                 while stack and stack[-1].isdigit():
-                    k = stack.pop()+k
+                    k = stack.pop() + k
                 k = int(k)
                 stack.append(k*substr)
         return "".join(stack)
-        
