11511e93b917b42a9de395bb1c105c5c795df3cd
Abhinav Gupta <abhinavgupta0907@gmail.com>
2025-03-03 12:08:43 -0700

    Time: 114 ms (21.87%) | Memory: 21.8 MB (47.90%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.py
@@ -1,25 +1,19 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        MOD = 10**9 + 7
+        MOD = 10 **9 + 7
         result = 0
-        #Brute Force
-        # for i in range(len(arr)):
-        #     minVal = arr[i]
-        #     for j in range(i,len(arr)):
-        #         minVal = min(minVal, arr[j])
-        #         result = (result + minVal)% MOD
-        # return result
+
         prevElement = [-1]*len(arr)
+        nextElement = [len(arr)]*len(arr)
+
         stack = []
         for i in range(len(arr)):
-            while stack and arr[stack[-1]]>=arr[i]:
+            while stack and arr[stack[-1]] >= arr[i]:
                 stack.pop()
             prevElement[i] = stack[-1] if stack else -1
             stack.append(i)
         
-        nextElement = [len(arr)]*len(arr)
         stack = []
-
         for i in range(len(arr)-1, -1, -1):
             while stack and arr[stack[-1]] > arr[i]:
                 stack.pop()
@@ -31,5 +25,4 @@ class Solution:
             right = nextElement[i] - i
             result += arr[i] * left * right
             result = result % MOD
-
         return result
