64dcce71b3e6d10d484799160a7e07d1c7f17742
Abhinav Gupta <abhinavgupta0907@gmail.com>
2025-03-18 10:13:40 -0600

    Time: 0 ms (100.00%) | Memory: 18.1 MB (25.55%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        
-        s_list = [0]*26
-        t_list = [0]*26
+        sDict = Counter(s)
+        tDict = Counter(t)
 
-        for i in range(len(s)):
-            s_list[ord(s[i])- ord('a')]+=1
-            t_list[ord(t[i]) - ord('a')]+=1
-        return s_list == t_list
+        return tDict == sDict
